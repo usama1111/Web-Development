ff71c39a43f791ed283e25b19831dbbae96602aa
Usama bantwa <51973041+usama1111@users.noreply.github.com>
2020-08-11 00:58:25 +0500

    Add files via upload

---
+++ JavaScript/Practice 1/java.js
@@ -0,0 +1,76 @@
+/*
+
+var number = prompt("enter a number")
+
+if(number == 18)
+{
+
+alert("You are eligible ");
+
+
+}
+
+else{
+
+alert("Sorry bro");
+
+}
+*/
+
+/*
+var students = ["usama","sohail","ashraf"];
+
+for(var i = 0; i < students.length; i++)
+{
+
+prompt("welcome " + students[i]);
+
+
+}
+
+*/
+
+/*
+var hell = ["usama","iqbal","shamshad"]
+
+
+//hell.splice(1,2)
+
+//hell.pop()
+
+hell.slice(0,1)
+
+console.log(hell)
+*/
+
+/*
+for(i = 0 ; i <= 10;i++)
+{
+
+
+console.log(i)
+
+}
+*/
+
+var number1 = prompt("Enter first number")
+var number2 = prompt("Enter second number")
+var operator = prompt("enter the operator")
+
+
+if(operator === "-")
+{
+
+console.log("the answer is " + (number1 - number2) )
+
+
+}
+
+
+
+
+
+
+
+
+
