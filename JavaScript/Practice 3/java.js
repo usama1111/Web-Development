19bb1a40cca4517862854e0c7f1147c9a2920277
Usama bantwa <51973041+usama1111@users.noreply.github.com>
2020-08-12 00:24:38 +0500

    Add files via upload

---
+++ JavaScript/Practice 3/java.js
@@ -0,0 +1,27 @@
+
+// While loop 
+
+
+// var i = prompt("enter a number")
+
+
+
+// while( i <= 10)
+// {
+
+// document.write(i);
+
+
+
+// i++;
+
+// }
+
+function clickbtn(){
+
+
+alert("bantoo baba")
+
+
+}
+
