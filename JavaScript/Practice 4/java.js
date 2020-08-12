8b6282b582c9c2198ac9f6d47436a8a56166f46d
Usama bantwa <51973041+usama1111@users.noreply.github.com>
2020-08-13 01:10:21 +0500

    Add files via upload

---
+++ JavaScript/Practice 4/java.js
@@ -0,0 +1,27 @@
+// function changeImage(id,src){
+
+
+
+// var image = document.getElementById(id);
+// image.src = src
+
+
+
+// }
+
+
+
+function changeImage(){
+
+
+
+    var image = document.getElementById('img');
+   image.style.width = '50%'    
+
+  
+    
+    }
+    
+
+
+
